c6e25361906e10346bbef0e348bd73d295fdb91b
AhmadAT86 <ah.aboutaouk@gmail.com>
2016-01-26 08:22:11 +0100

    Test

---
+++ WebApplication.CombiTech/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication.CombiTech.Startup))]
+namespace WebApplication.CombiTech
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
